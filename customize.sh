89775955250a66dfe2ee3e7452b9884058185a1c
chuwendan123 <70813443+chuwendan123@users.noreply.github.com>
2023-12-23 21:15:53 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 #===============================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.1.252/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' openwrt/package/base-files/files/bin/config_generate
 
+git clone https://github.com/kenzok8/openwrt-packages.git package/openwrt-packages
+
+sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
 #2. Custom settings
 #sed -i 's?zstd$?zstd ucl upx\n$(curdir)/upx/compile := $(curdir)/ucl/compile?g' tools/Makefile
 #sed -i 's/$(TARGET_DIR)) install/$(TARGET_DIR)) install --force-overwrite/' package/Makefile
